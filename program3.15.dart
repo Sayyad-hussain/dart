003d5f7e7dee6540d47a4eb04d18cdfa2dbcb621
Sayyad Hussain <sayyadhussain315@gmail.com>
2024-07-25 21:46:10 +0500

    program 3.8 change

---
+++ program3.15.dart
@@ -0,0 +1,7 @@
+//Write a dart program that inputs total marks and obtained marks. and finds its percentage of the obtained marks;
+import 'dart:io';
+void main()
+{
+  stdout.write("please enter a number: ");
+  int marks = stdin.readlin
+}
