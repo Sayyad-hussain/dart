b93a03697fcb071b345f300ed87eb12c7536c42f
Sayyad Hussain <sayyadhussain315@gmail.com>
2026-03-03 22:27:05 +0500

    20260303

---
+++ Replace_Substring.dart
@@ -0,0 +1,5 @@
+void main(){
+    String gfg = "Welcome GeeksForGeeks";
+    String result = gfg.replaceAll("GeeksForGeeks", "Geek!").replaceAll("!", " :)");
+    print(result);
+}
---
+++ String_isEmpty.dart
@@ -0,0 +1,6 @@
+void main(){
+  String str1 = "Geeksforgeeks";
+  String str2 = "";
+  print(str1.isEmpty);
+  print(str2.isEmpty);
+}
