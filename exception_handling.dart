0b3c44f0d003c9ec0d191b282a678adc56ac9da9
Sayyad Hussain <sayyadhussain315@gmail.com>
2026-03-06 23:36:20 +0500

    20260306

---
+++ exception_handling.dart
@@ -0,0 +1,32 @@
+//Exception Handling
+/*
+void main() {
+  String geek = "GeeksForGeeks";
+  try {
+    int result = int.parse(geek);
+  } on FormatException {
+    print('Error!! \nCan\'t act as input is not an integer.');
+  }
+}
+ */
+
+//
+// void main()
+// {
+//   int x = 10;
+//   //double  result = 0.0;
+//   try
+//       {
+//         int result = x ~/ 0 ;
+//          print(result);
+//       }
+//     catch (e)
+//   {
+//     print(e);
+//   }
+//   //print(result);
+// }
+
+
+
+
